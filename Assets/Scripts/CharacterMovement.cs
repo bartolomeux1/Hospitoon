f674e8d38a95b7b4526811cab9b5360369e3faef
Dani <39123021+bartolomeux1@users.noreply.github.com>
2024-08-30 18:10:17 -0300

    Movimento

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    private CharacterController character;
+    private Vector3 inputX;
+
+    public float velocity = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = GetComponent<CharacterController>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputX = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        inputX = Vector3.ClampMagnitude(inputX, 1);
+        character.Move(inputX * velocity * Time.deltaTime);
+    }
+}
